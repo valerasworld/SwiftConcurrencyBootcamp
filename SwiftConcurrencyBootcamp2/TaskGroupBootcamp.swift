911a3744ffed869753818207fcb85eb674aaa067
Valery Zazulin <zazulinvalerii@gmail.com>
2024-01-15 01:25:41 +0100

    6. TaskGroup

---
+++ SwiftConcurrencyBootcamp2/TaskGroupBootcamp.swift
@@ -0,0 +1,113 @@
+//
+//  TaskGroupBootcamp.swift
+//  SwiftConcurrencyBootcamp2
+//
+//  Created by Валерий Зазулин on 15.01.2024.
+//
+
+import SwiftUI
+
+// Scalable method to concurrently get data of the same type at the same time
+class TaskGroupBootcampDataManager {
+    
+    func fetchImagesWithAsyncLet() async throws -> [UIImage] {
+        let urlString = "https://picsum.photos/600"
+
+        async let fetchImage1 = fetchImage(urlString: urlString)
+        async let fetchImage2 = fetchImage(urlString: urlString)
+        async let fetchImage3 = fetchImage(urlString: urlString)
+        async let fetchImage4 = fetchImage(urlString: urlString)
+        
+        let (image1, image2, image3, image4) = await (try fetchImage1, try fetchImage2, try fetchImage3, try fetchImage4)
+        
+        return [image1, image2, image3, image4]
+    }
+    
+    func fetchImagesWithTaskGroup() async throws -> [UIImage] {
+        let urlStrings = [
+            "https://picsum.photos/600",
+            "https://picsum.photos/600",
+            "https://picsum.photos/600",
+            "https://picsum.photos/600",
+            "https://picsum.photos/600"
+        ]
+        
+        return try await withThrowingTaskGroup(of: UIImage?.self) { group in
+            var images: [UIImage] = []
+            images.reserveCapacity(urlStrings.count)
+            for urlString in urlStrings {
+                group.addTask {
+                    try? await self.fetchImage(urlString: urlString)
+                }
+            }
+            
+            for try await image in group {
+                if let image = image {
+                    images.append(image)
+                }
+            }
+            
+            return images
+        }
+    }
+    
+    private func fetchImage(urlString: String) async throws -> UIImage {
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let image = UIImage(data: data) {
+                return image
+            } else {
+                throw URLError(.badURL)
+            }
+        } catch {
+            throw error
+        }
+    }
+    
+}
+
+class TaskGroupBootcampViewModel: ObservableObject {
+   
+    @Published var images: [UIImage] = []
+    let manager = TaskGroupBootcampDataManager()
+    
+    func getImages() async {
+        if let images = try? await manager.fetchImagesWithTaskGroup() {
+            self.images.append(contentsOf: images)
+        }
+    }
+    
+}
+
+struct TaskGroupBootcamp: View {
+    
+    @StateObject private var viewModel = TaskGroupBootcampViewModel()
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(viewModel.images, id: \.self) { image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 150)
+                    }
+                }
+            }
+            .navigationTitle("Task Group 🥳")
+            .task {
+                await viewModel.getImages()
+            }
+        }
+    }
+}
+
+#Preview {
+    TaskGroupBootcamp()
+}
