f440316e805f45e32f539175cdbb52259dc7b979
Valery Zazulin <zazulinvalerii@gmail.com>
2024-01-20 00:19:01 +0100

    17. PhotoPicker in SwiftUI and PhotosUI

--- SwiftConcurrencyBootcamp2/PhotoPickerBootcamp.swift
+++ SwiftConcurrencyBootcamp2/PhotoPickerBootcamp.swift
@@ -6,10 +6,107 @@
 //
 
 import SwiftUI
+import PhotosUI
+
+@MainActor
+final class PhotoPickerViewModel: ObservableObject {
+    
+    @Published private(set) var selectedImage: UIImage? = nil
+    @Published var imageSelection: PhotosPickerItem? = nil {
+        didSet {
+            setImage(from: imageSelection)
+        }
+    }
+    
+    @Published private(set) var selectedImages: [UIImage] = []
+    @Published var imageSelections: [PhotosPickerItem] = [] {
+        didSet {
+            setImages(from: imageSelections)
+        }
+    }
+    
+    private func setImage(from selection: PhotosPickerItem?) {
+        guard let selection else { return }
+        
+        Task {
+            if let data = try await selection.loadTransferable(type: Data.self) {
+                if let uiImage = UIImage(data: data) {
+                    selectedImage = uiImage
+                    return
+                }
+            }
+            // Catch Errors way:
+//            do {
+//                let data = try await selection.loadTransferable(type: Data.self)
+//                
+//                guard let data, let uiImage = UIImage(data: data) else {
+//                    throw URLError(.badServerResponse)
+//                }
+//                selectedImage = UIImage(data: data)
+//            } catch {
+//                print(error)
+//            }
+        }
+    }
+    
+    private func setImages(from selections: [PhotosPickerItem]) {
+        
+        Task {
+            var images:  [UIImage] = []
+            for selection in selections {
+                if let data = try? await selection.loadTransferable(type: Data.self) {
+                    if let uiImage = UIImage(data: data) {
+                        images.append(uiImage)
+                    }
+                }
+            }
+            selectedImages = images
+        }
+    }
+    
+}
 
 struct PhotoPickerBootcamp: View {
+    
+    @StateObject private var viewModel = PhotoPickerViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 40) {
+            Text("Placeholder")
+            
+            if let image = viewModel.selectedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 200)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+            
+            PhotosPicker(selection: $viewModel.imageSelection, matching: .images) {
+                Text("Open the photo picker")
+                    .foregroundStyle(Color.red)
+            }
+            
+            if !viewModel.selectedImages.isEmpty {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(viewModel.selectedImages, id: \.self) { image in
+                            Image(uiImage: image)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 75, height: 75)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
+                    }
+                }
+            }
+            
+            PhotosPicker(selection: $viewModel.imageSelections, matching: .images) {
+                Text("Open the multiple photos picker")
+                    .foregroundStyle(Color.red)
+            }
+            
+        }
     }
 }
 
